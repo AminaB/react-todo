4b824896b10c140762132fd3e8d59b6057169f29
AminaB <aminatou.barry97@gmail.com>
2025-04-24 00:11:30 +0200

    refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,11 @@
 import './App.css'
+import Todos from "./components/Todos.jsx";
 
 function App() {
-const todos=[
-    {id:"1", title:"dev", description:"dev the app", completed:false},
-    {id:"2", title:"test", description:"test the app", completed:false},
-    {id:"3", title:"deploy", description:"deploy the app", completed:false},
-];
+
   return (
     <>
-      <div className="container">
-
-          <table className="table">
-              <thead>
-              <tr>
-                  <th>id</th>
-                  <th>title</th>
-                  <th>description</th>
-                  <th>status</th>
-              </tr>
-              </thead>
-              <tbody>
-              {todos.map((todo)=>
-                  (
-                      <tr>
-                          <td>{todo.id}</td>
-                          <td>{todo.title}</td>
-                          <td>{todo.description}</td>
-                          {todo.completed
-                              ? <td>Done</td>
-                              :<td className="bg-primary">TO DO</td>
-                          }
-
-                      </tr>
-                  )
-
-              )}
-              </tbody>
-          </table>
-
-      </div>
-
+    <Todos />
     </>
   )
 }
---
+++ src/components/Todos.jsx
@@ -0,0 +1,39 @@
+function Todos(){
+    const todos=[
+        {id:"1", title:"dev", description:"dev the app", completed:false},
+        {id:"2", title:"test", description:"test the app", completed:false},
+        {id:"3", title:"deploy", description:"deploy the app", completed:false},
+    ];
+    return (
+        <>
+            <table className="table">
+                <thead>
+                <tr>
+                    <th>id</th>
+                    <th>title</th>
+                    <th>description</th>
+                    <th>status</th>
+                </tr>
+                </thead>
+                <tbody>
+                {todos.map((todo)=>
+                    (
+                        <tr>
+                            <td>{todo.id}</td>
+                            <td>{todo.title}</td>
+                            <td>{todo.description}</td>
+                            {todo.completed
+                                ? <td>Done</td>
+                                :<td className="bg-primary">TO DO</td>
+                            }
+
+                        </tr>
+                    )
+
+                )}
+                </tbody>
+            </table>
+        </>
+    )
+}
+export default Todos;
